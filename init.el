036f0cb6651cf14fe669b8ca3771733dd7aa1099
Todd Fiala <todd.fiala@gmail.com>
2014-10-28 21:04:20 -0700

    Move ocaml setup to separate method; add ido.last to .gitignore.
    The ocaml setup currently doesn't happen now.  Need to make it
    smart enough to figure out if ocaml is installed.

--- init.el
+++ init.el
@@ -98,37 +98,39 @@
 ;; gdb-mode fix for long load time on Emacs 24.
 (setq gdb-create-source-file-list nil)
 
-;; OCaml Mode
-(require 'tuareg)
-(setq auto-mode-alist
-      (append '(("\\.ml[ily]?$" . tuareg-mode))
-              auto-mode-alist))
-;; Tweak for problem on OS X where Emacs.app doesn't run the right
-;; init scripts when invoking a sub-shell
-(cond
- ((eq window-system 'ns) ; macosx
-  ;; Invoke login shells, so that .profile or .bash_profile is read
-  (setq shell-command-switch "-lc")))
-;; Setup environment variables using opam
-(defun opam-vars ()
-  (let* ((x (shell-command-to-string "opam config env"))
-         (x (split-string x "\n"))
-         (x (remove-if (lambda (x) (equal x "")) x))
-         (x (mapcar (lambda (x) (split-string x ";")) x))
-         (x (mapcar (lambda (x) (car x)) x))
-         (x (mapcar (lambda (x) (split-string x "=")) x))
-         )
-    x))
-(dolist (var (opam-vars))
-  (setenv (car var) (substring (cadr var) 1 -1)))
-(setq exec-path (split-string (getenv "PATH") path-separator))
-;; Update the emacs load path
-(push (concat (getenv "OCAML_TOPLEVEL_PATH")
-              "/../../share/emacs/site-lisp") load-path)
-;; Automatically load utop.el
-(autoload 'utop "utop" "Toplevel for OCaml" t)
-(autoload 'utop-setup-ocaml-buffer "utop" "Toplevel for OCaml" t)
-(add-hook 'tuareg-mode-hook 'utop-setup-ocaml-buffer)
+;; OCaml support
+;; FIXME move to 'tfiala-ocaml-config
+(defun setup-ocaml-mode ()
+  (require 'tuareg)
+  (setq auto-mode-alist
+        (append '(("\\.ml[ily]?$" . tuareg-mode))
+                auto-mode-alist))
+  ;; Tweak for problem on OS X where Emacs.app doesn't run the right
+  ;; init scripts when invoking a sub-shell
+  (cond
+   ((eq window-system 'ns) ; macosx
+    ;; Invoke login shells, so that .profile or .bash_profile is read
+    (setq shell-command-switch "-lc")))
+  ;; Setup environment variables using opam
+  (defun opam-vars ()
+    (let* ((x (shell-command-to-string "opam config env"))
+           (x (split-string x "\n"))
+           (x (remove-if (lambda (x) (equal x "")) x))
+           (x (mapcar (lambda (x) (split-string x ";")) x))
+           (x (mapcar (lambda (x) (car x)) x))
+           (x (mapcar (lambda (x) (split-string x "=")) x))
+           )
+      x))
+  (dolist (var (opam-vars))
+    (setenv (car var) (substring (cadr var) 1 -1)))
+  (setq exec-path (split-string (getenv "PATH") path-separator))
+  ;; Update the emacs load path
+  (push (concat (getenv "OCAML_TOPLEVEL_PATH")
+                "/../../share/emacs/site-lisp") load-path)
+  ;; Automatically load utop.el
+  (autoload 'utop "utop" "Toplevel for OCaml" t)
+  (autoload 'utop-setup-ocaml-buffer "utop" "Toplevel for OCaml" t)
+  (add-hook 'tuareg-mode-hook 'utop-setup-ocaml-buffer))
 
 (require 'tfiala-slime-config)
 
