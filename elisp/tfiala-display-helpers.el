d7fe3721ff1b6b504b66c6c3cc82b199d1759f08
Todd Fiala <tfiala@apple.com>
2015-04-18 21:00:04 -0700

    Only set up toggling of solarized background with GUI.

--- elisp/tfiala-display-helpers.el
+++ elisp/tfiala-display-helpers.el
@@ -1,21 +1,25 @@
 (require 'cl-lib)
 
-(add-hook 'after-make-frame-functions
-          (lambda (frame)
-	    (let ((solarized-color (or (getenv "SOLARIZED") "light")))
-	      (message (concat "setting solarized: " solarized-color " (getenv \"SOLARIZED\") = " (getenv "SOLARIZED")))
-	      (set-frame-parameter frame 'background-mode (intern solarized-color))
-	      (enable-theme 'solarized))))
+(when window-system
+  ;; This hook ensures that frames default to solarized light, but
+  ;; will respect the SOLARIZED environment variable to select dark if
+  ;; specified.  It only makes sense when we're in windowed mode (gui).
+  (add-hook 'after-make-frame-functions
+            (lambda (frame)
+              (let ((solarized-color (or (getenv "SOLARIZED") "light")))
+                (message (concat "setting solarized: " solarized-color " (getenv \"SOLARIZED\") = " (getenv "SOLARIZED")))
+                (set-frame-parameter frame 'background-mode (intern solarized-color))
+                (enable-theme 'solarized))))
 
-(defun tfiala-toggle-background-mode ()
-  "Toggle the background mode between light and dark."
-  (interactive
-   (cl-flet ((new-color-from-old (old-color)
-                              (cond ((eq old-color 'light) 'dark)
-                                    (t 'light))))
-     (let ((new-color (new-color-from-old (or (frame-parameter nil 'background-mode) 'light))))
-       (message (concat "setting background-mode: " (symbol-name new-color)))
-       (set-frame-parameter nil 'background-mode new-color)
-       (enable-theme 'solarized)))))
+  (defun tfiala-toggle-background-mode ()
+    "Toggle the background mode between light and dark."
+    (interactive
+     (cl-flet ((new-color-from-old (old-color)
+                                   (cond ((eq old-color 'light) 'dark)
+                                         (t 'light))))
+       (let ((new-color (new-color-from-old (or (frame-parameter nil 'background-mode) 'light))))
+         (message (concat "setting background-mode: " (symbol-name new-color)))
+         (set-frame-parameter nil 'background-mode new-color)
+         (enable-theme 'solarized))))))
 
 (provide 'tfiala-display-helpers)
