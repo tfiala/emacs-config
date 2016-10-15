85aa1c1d4de8946828f9aadc432d15bcdcec4cf6
Todd Fiala <todd.fiala@gmail.com>
2016-10-14 23:50:30 -0700

    started minimal emacs config (i.e. v2)
    This is a bare-bones simple emacs config that supports the following:
    * solarized, light and dark (via SOLARIZED env var, defaults to light)
    * clojure development (cider, rainbow parens, paredit)
    * helm for M-x, file-find and other simple bits

--- elisp/tfiala-spell-config.el
+++
@@ -1,9 +0,0 @@
-;; Find the first aspell
-(let* ((search-paths `("/usr/bin" ,(concat (getenv "HOME") "/homebrew/bin") "/usr/local/bin"))
-       (ispell-path (or
-                     (tfiala-first-file-in-path search-paths "aspell")
-                     (tfiala-first-file-in-path search-paths "aspell"))))
-  (when ispell-path
-    (setq-default ispell-program-name ispell-path)))
-
-(provide 'tfiala-spell-config)
--- init.el
+++ init.el
@@ -1,230 +1,118 @@
-(require 'cl-lib)
-
-;; add our elisp dir
-(let ((elisp-dir (concat (file-name-directory load-file-name) "elisp")))
-  (add-to-list 'load-path elisp-dir))
-
-(require 'tfiala-bootstrap)
-(require 'tfiala-package)
-(require 'tfiala-keyboard)
-
 ;;
-;; load desired packages
-;;
-;; Note: this becomes a do nothing if emacs version
-;; is less than 24 and there is no local package.el
-;; found in this directory: $HOME/emacs-vc
-;;
-(let ((always-load-packages '(cider
-                              ac-cider
-                              clojure-mode
-			      color-theme
-                              color-theme-solarized
-                              company
-                              company-ghc
-                              exec-path-from-shell
-                              flx-ido
-                              flycheck
-                              flycheck-haskell
-                              ghc
-                              haskell-mode
-                              hi2
-			      magit
-			      org
-                              projectile
-                              rainbow-delimiters
-                              rainbow-mode
-                              slime
-                              tuareg))
-      (additional-packages
-       (cond
-	((>= emacs-major-version 24) '(starter-kit
-				       starter-kit-lisp
-				       starter-kit-eshell))
-	(t '()))))
-
-  (tfiala-load-package-list (append additional-packages always-load-packages)))
-
-;; Before we do anything else, ensure our path is set correctly.  For
-;; now just do this on MacOSX.
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize)
-  (exec-path-from-shell-copy-env "EDITOR"))
-
-(tfiala-per-machine-pre)
-
-;; minimal keyboard setup
-(when (and (boundp 'tfiala-keyboard-use-kinesis)
-           tfiala-keyboard-use-kinesis)
-  (tfiala-setup-kinesis-keyboard))
-
-;; Windows movement helpers
-;; Ctrl + Super + hjkl moves to a window in that direction.
-(global-set-key (kbd "C-s-h") 'windmove-left)
-(global-set-key (kbd "C-s-l") 'windmove-right)
-(global-set-key (kbd "C-s-k") 'windmove-up)
-(global-set-key (kbd "C-s-j") 'windmove-down)
-
-;;
-;; Dired settings
+;; Load packages 
 ;;
 
-;; Fixup dired warning re: ls on OS X
-(when (eq system-type 'darwin)
-  (setq dired-use-ls-dired nil)
-  )
+;; setup package loading
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
 
-;; Use dired-x
-(add-hook
- 'dired-load-hook
- (lambda ()
-   (load "dired-x")
-   ;; Set dired-x global variables here.  For example:
-   ;; (setq dired-guess-shell-gnutar "gtar")
-   ;; (setq dired-x-hands-off-my-keys nil)
-   ))
-(add-hook
- 'dired-mode-hook
- (lambda ()
-   ;; Set dired-x buffer-local variables here.
-   (dired-omit-mode 1)
-   ))
+(require 'package)
+(package-initialize)
+(setq package-enable-at-startup nil)
 
-;; Enable dired-jump and dired-jump-other-window before
-;; dired is loaded.
-(autoload 'dired-jump "dired-x"
-  "Jump to Dired buffer corresponding to current buffer." t)
+;; Ensure we only refresh package contents when we don't already
+;; have any (i.e. first install); otherwise, only update on user
+;; timeline.
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
-(autoload 'dired-jump-other-window "dired-x"
-  "Like \\[dired-jump] (dired-jump) but in other window." t)
+;; Install packages.
+(let ((packages
+       '(cider
+	 clojure-mode
+	 color-theme
+	 helm
+	 magit
+	 org
+	 paredit
+	 rainbow-delimiters
+	 solarized-theme
+	 )))
+  (dolist (p packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
 
-(define-key global-map "\C-x\C-j" 'dired-jump)
-(define-key global-map "\C-x4\C-j" 'dired-jump-other-window)
+;;
+;; Setup fonts
+;;
 
-;; Enable projectile mode everywhere
-(projectile-global-mode)
-
-;; set default font
 (when window-system
-  (let* ((font-size (or (and (boundp 'tfiala-fontsize)
-                            tfiala-fontsize)
-                        12))
+  (let* ((font-size
+	  (or (and (boundp 'tfiala-fontsize) tfiala-fontsize)
+	      16))
          (font-name (format "Source Code Pro-%d" font-size)))
     (print (concat "using font " font-name))
     (add-to-list 'default-frame-alist `(font . ,font-name))))
 
 ;;
-;; visual tweaks
-;;
-;; Some of these settings are redundant if the starter-kit packages
-;; are available and loaded.  This covers us in the case that the
-;; starter kit packages are not available.
+;; Hide welcome screen
 ;;
 
-(transient-mark-mode 1)
-
-;; move scroll bars to right
-(when window-system
-  (set-scroll-bar-mode 'right))
-
-;; disable splashscreen
-(setq inhibit-splash-screen t)
-
-;; ido mode
-(require 'ido)
-(ido-mode t)
-
-;; always use y or n for yes-or-no questions
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;; compiler output tracking
-(setq compilation-scroll-output t)
-
-;; disable the ugly visible bell
-(setq visible-bell nil)
-
-;; set ispell
-(require 'tfiala-spell-config)
-
-(require 'tfiala-org-config)
-
-;; start the server if it's not running
-(require 'server)
-(if (and (fboundp 'server-running-p)
-         (not (server-running-p)))
-    (server-start))
-
-;; whitespace handling
-(require 'whitespace)
-(global-whitespace-mode)
-
-;; gdb-mode fix for long load time on Emacs 24.
-(setq gdb-create-source-file-list nil)
-
-;; OCaml support
-;; FIXME move to 'tfiala-ocaml-config
-(defun setup-ocaml-mode ()
-  (require 'tuareg)
-  (setq auto-mode-alist
-        (append '(("\\.ml[ily]?$" . tuareg-mode))
-                auto-mode-alist))
-  ;; Tweak for problem on OS X where Emacs.app doesn't run the right
-  ;; init scripts when invoking a sub-shell
-  (cond
-   ((eq window-system 'ns) ; macosx
-    ;; Invoke login shells, so that .profile or .bash_profile is read
-    (setq shell-command-switch "-lc")))
-  ;; Setup environment variables using opam
-  (defun opam-vars ()
-    (let* ((x (shell-command-to-string "opam config env"))
-           (x (split-string x "\n"))
-           (x (cl-remove-if (lambda (x) (equal x "")) x))
-           (x (mapcar (lambda (x) (split-string x ";")) x))
-           (x (mapcar (lambda (x) (car x)) x))
-           (x (mapcar (lambda (x) (split-string x "=")) x))
-           )
-      x))
-  (dolist (var (opam-vars))
-    (setenv (car var) (substring (cadr var) 1 -1)))
-  (setq exec-path (split-string (getenv "PATH") path-separator))
-  ;; Update the emacs load path
-  (push (concat (getenv "OCAML_TOPLEVEL_PATH")
-                "/../../share/emacs/site-lisp") load-path)
-  ;; Automatically load utop.el
-  (autoload 'utop "utop" "Toplevel for OCaml" t)
-  (autoload 'utop-setup-ocaml-buffer "utop" "Toplevel for OCaml" t)
-  (add-hook 'tuareg-mode-hook 'utop-setup-ocaml-buffer))
-
-(require 'tfiala-clojure-config)
-(require 'tfiala-slime-config)
-
-(require 'tfiala-haskell-config)
+(setq inhibit-startup-screen t)
 
 ;;
-;; python setup
+;; No toolbar
 ;;
 
-;; Use flycheck.
-(add-hook 'python-mode-hook 'flycheck-mode)
-
-;; After flake8-ing, use pylint.
-(with-eval-after-load "flycheck"
-  (print "-tfiala- setting up python flychecker chain")
-  (flycheck-add-next-checker 'python-flake8
-                             'python-pylint))
-
-;; Enable company-mode everywhere (complete-any mode).
-(global-company-mode)
-
-;; Accounting related.
-(require 'tfiala-beancount-config)
-
-(require 'tfiala-display-helpers)
+(tool-bar-mode -1)
 
 ;;
-;; KEEP THIS AT THE BOTTOM OF THE init.el FILE.
+;; Setup color theme
 ;;
-;; This is the hook whereby per-machine configuration can adjust any
-;; of the defaults before emacs initialization completes.
+
+(require 'color-theme)
+(color-theme-initialize)
+(when (not window-system)
+  (let ((solarized-color-string (or (getenv "SOLARIZED") "light")))
+    (setq frame-background-mode (intern solarized-color-string))
+    (set-terminal-parameter nil 'background-mode (intern solarized-color-string))))
+(if (string= (or (getenv "SOLARIZED") "light") "light")
+    (load-theme 'solarized-light t)
+  (load-theme 'solarized-dark t))
+
+(load-theme 'solarized t)
+
 ;;
-(tfiala-per-machine-post)
+;; Setup dired
+;;
+
+(setq insert-directory-program "/usr/local/bin/gls")
+(setq dired-listing-switches "-aBhl --group-directories-first")
+
+;;
+;; Setup paredit
+;;
+
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(let ((lispen-hooks
+       '(emacs-lisp-mode-hook
+	 eval-expression-minibuffer-setup-hook
+	 lisp-mode-hook
+	 lisp-interaction-mode-hook
+	 scheme-mode-hook)))
+  (seq-do
+   #'(lambda (x) (add-hook x #'enable-paredit-mode))
+   lispen-hooks))
+
+;;
+;; Setup rainbow delimiters (rainbow parens)
+;;
+
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+;;
+;; Always show matching paren
+;;
+
+(show-paren-mode 1)
+
+;;
+;; Setup helm
+;;
+(require 'helm-config)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
